fd2f8c1f80fc7814cc4ac160dde12f8fba2b0918
Elysia <a.elysia@proton.me>
2025-12-19 23:23:43 +0800

    build: update type2-runtime source URL

--- ci/build.sh
+++ ci/build.sh
@@ -43,7 +43,7 @@ cp "$(which zsyncmake)" AppDir/usr/bin
 cp "$repo_root"/resources/AppRun.sh AppDir/AppRun
 chmod +x AppDir/AppRun
 
-wget https://github.com/AppImage/type2-runtime/releases/download/continuous/runtime-"$ARCH"
+wget https://github.com/loong64/type2-runtime/releases/download/continuous/runtime-"$ARCH"
 
 pushd AppDir
 ln -s usr/share/applications/appimagetool.desktop .
